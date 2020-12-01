c85b2a4704809551a6ee5a2d59439a390ad0749a
NEKOLIKOL <nicolescayambas@gmail.com>
2020-12-01 17:37:32 +0800

    added a main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+var loggedIn = false;
+    function bought() {
+        if(loggedIn==false) {
+            alert("You need to log in first.");
+        }
+    }
+    function addedToCart() {
+            alert("Added to Cart.");
+    }
